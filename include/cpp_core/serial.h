ff62ee21db9a2fefc35e311af181168450085ded
Katze719 <38188106+Katze719@users.noreply.github.com>
2025-07-06 18:36:39 +0200

    Apply suggestion from @Mqxx
    Co-authored-by: Mqx <62719703+Mqxx@users.noreply.github.com>

--- include/cpp_core/serial.h
+++ include/cpp_core/serial.h
@@ -68,7 +68,7 @@ extern "C"
 
     MODULE_API int serialWriteLine(int64_t handle, const void* buffer, int bufferSize, int timeoutMs);
 
-    MODULE_API int serialReadUntilSequence(int64_t handle, void* buffer, int bufferSize, int timeout /*ms*/, void* sequence);
+    MODULE_API int serialReadUntilSequence(int64_t handle, void* buffer, int bufferSize, int timeoutMs, void* sequence);
 
     MODULE_API int serialReadFrame(int64_t handle, void* buffer, int bufferSize, int timeout /*ms*/, char startByte, char endByte);
 
