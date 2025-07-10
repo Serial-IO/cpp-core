eaf9c545a2e87d640977671d83281ead40f9ee2a
Katze719 <pauldorn1234@gmail.com>
2025-07-10 21:38:47 +0200

    Update .gitignore and CMakeLists.txt; refine documentation in serial interface headers
    - Added `.cache/` to `.gitignore` to exclude cache files from version control.
    - Removed unnecessary whitespace in `CMakeLists.txt` for cleaner formatting.
    - Clarified return value documentation in `serial_drain.h` and `serial_open.h` to improve understanding of function outcomes.
    
    These changes enhance project organization and improve code documentation clarity.

--- include/cpp_core/interface/serial_open.h
+++ include/cpp_core/interface/serial_open.h
@@ -17,13 +17,13 @@ extern "C"
      *   - `const wchar_t*` on Windows.
      *
      * @param port Null-terminated device identifier (e.g. "COM3", "/dev/ttyUSB0"). Passing `nullptr` results in
-     * ::cpp_core::StatusCodes::kNotFoundError.
+     * a failure.
      * @param baudrate Desired baud rate in bit/s (≥ 300).
      * @param data_bits Number of data bits (5–8).
      * @param parity 0 = none, 1 = even, 2 = odd.
      * @param stop_bits 0 = 1 stop bit, 2 = 2 stop bits.
      * @param error_callback [optional] Callback to invoke on error. Defined in error_callback.h. Default is `nullptr`.
-     * @return A positive opaque handle on success or a negative value from ::cpp_core::StatusCodes on failure.
+     * @return A positive opaque handle on success or a negative value on failure.
      */
     MODULE_API auto serialOpen(
         void          *port,
