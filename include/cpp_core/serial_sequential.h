9f12e63687287940a0b58366756af718ebcc2b22
Katze719 <pauldorn1234@gmail.com>
2025-07-16 21:27:21 +0200

    Refactor serial_sequential header to aggregate sequential interface headers
    - Replaced individual sequential header inclusions in `serial_sequential.h` with a single inclusion of `sequential.h` for improved organization.
    - Introduced a new `sequential.h` header that consolidates all sequential interface headers, enhancing maintainability and accessibility.
    
    These changes streamline the header management in the serial interface, aligning with ongoing refactoring efforts.

--- include/cpp_core/serial_sequential.h
+++ include/cpp_core/serial_sequential.h
@@ -11,19 +11,5 @@
 #include "interface/serial_set_read_callback.h"
 #include "interface/serial_set_write_callback.h"
 
-// Aggregates all *Sequential headers
-#include "interface/sequential/serial_abort_read_sequential.h"
-#include "interface/sequential/serial_abort_write_sequential.h"
-#include "interface/sequential/serial_clear_buffer_in_sequential.h"
-#include "interface/sequential/serial_clear_buffer_out_sequential.h"
-#include "interface/sequential/serial_close_sequential.h"
-#include "interface/sequential/serial_drain_sequential.h"
-#include "interface/sequential/serial_in_bytes_total_sequential.h"
-#include "interface/sequential/serial_in_bytes_waiting_sequential.h"
-#include "interface/sequential/serial_out_bytes_total_sequential.h"
-#include "interface/sequential/serial_out_bytes_waiting_sequential.h"
-#include "interface/sequential/serial_read_line_sequential.h"
-#include "interface/sequential/serial_read_sequential.h"
-#include "interface/sequential/serial_read_until_sequence_sequential.h"
-#include "interface/sequential/serial_read_until_sequential.h"
-#include "interface/sequential/serial_write_sequential.h"
+// Sequential wrappers
+#include "interface/sequential.h"
