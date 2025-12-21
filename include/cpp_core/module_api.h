5a1fb20d4af9f658048515b6f10ad4d32cf4bb51
Katze719 <pauldorn1234@gmail.com>
2025-12-21 09:26:27 +0100

    Fix export macro name for Windows bindings in module_api.h

--- include/cpp_core/module_api.h
+++ include/cpp_core/module_api.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #if defined(_WIN32) || defined(__CYGWIN__)
-#ifdef cpp_windows_bindings_EXPORTS
+#ifdef cpp_bindings_windows_EXPORTS
 #define MODULE_API __declspec(dllexport)
 #else
 #define MODULE_API __declspec(dllimport)
