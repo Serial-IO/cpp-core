1616b1c00e5a4f78248e2e548dd8d689f62a7ab4
Katze719 <pauldorn1234@gmail.com>
2026-03-17 22:22:46 +0100

    Refactor status code definitions in status_codes.h
    - Changed ValueType from std::int32_t to std::int64_t for improved range.
    - Replaced the make function with a GenCode template structure for generating error codes, enhancing clarity and maintainability.
    - Updated error codes in Configuration, Connection, Io, Control, and Monitor categories to utilize the new GenCode structure.

--- include/cpp_core/status_codes.h
+++ include/cpp_core/status_codes.h
@@ -7,18 +7,23 @@
 namespace cpp_core
 {
 
-using ValueType = std::int32_t;
+using ValueType = std::int64_t;
 
 template <ValueType Category> struct CategoryBase
 {
   private:
     constexpr CategoryBase() = default;
 
-  public:
-    static constexpr auto make(ValueType detail) -> ValueType
+  protected:
+    static constexpr ValueType kCategoryMultiplier = 100;
+
+    template <ValueType Code> struct GenCode
     {
-        return -((Category * 100) + detail);
-    }
+        static constexpr ValueType kValue = -((Category * kCategoryMultiplier) + Code);
+    };
+
+  public:
+    static constexpr auto kCategoryCode = Category;
 };
 
 struct StatusCode
@@ -27,45 +32,45 @@ struct StatusCode
 
     struct Configuration : CategoryBase<1>
     {
-        static constexpr ValueType kSetBaudrateError = make(0);
-        static constexpr ValueType kSetDataBitsError = make(1);
-        static constexpr ValueType kSetParityError = make(2);
-        static constexpr ValueType kSetStopBitsError = make(3);
-        static constexpr ValueType kSetFlowControlError = make(4);
-        static constexpr ValueType kSetTimeoutError = make(5);
+        static constexpr ValueType kSetBaudrateError = GenCode<0>::kValue;
+        static constexpr ValueType kSetDataBitsError = GenCode<1>::kValue;
+        static constexpr ValueType kSetParityError = GenCode<2>::kValue;
+        static constexpr ValueType kSetStopBitsError = GenCode<3>::kValue;
+        static constexpr ValueType kSetFlowControlError = GenCode<4>::kValue;
+        static constexpr ValueType kSetTimeoutError = GenCode<5>::kValue;
     };
 
     struct Connection : CategoryBase<2>
     {
-        static constexpr ValueType kNotFoundError = make(0);
-        static constexpr ValueType kInvalidHandleError = make(1);
-        static constexpr ValueType kCloseHandleError = make(2);
+        static constexpr ValueType kNotFoundError = GenCode<0>::kValue;
+        static constexpr ValueType kInvalidHandleError = GenCode<1>::kValue;
+        static constexpr ValueType kCloseHandleError = GenCode<2>::kValue;
     };
 
     struct Io : CategoryBase<3>
     {
-        static constexpr ValueType kReadError = make(0);
-        static constexpr ValueType kWriteError = make(1);
-        static constexpr ValueType kAbortReadError = make(2);
-        static constexpr ValueType kAbortWriteError = make(3);
-        static constexpr ValueType kBufferError = make(4);
-        static constexpr ValueType kClearBufferInError = make(5);
-        static constexpr ValueType kClearBufferOutError = make(6);
+        static constexpr ValueType kReadError = GenCode<0>::kValue;
+        static constexpr ValueType kWriteError = GenCode<1>::kValue;
+        static constexpr ValueType kAbortReadError = GenCode<2>::kValue;
+        static constexpr ValueType kAbortWriteError = GenCode<3>::kValue;
+        static constexpr ValueType kBufferError = GenCode<4>::kValue;
+        static constexpr ValueType kClearBufferInError = GenCode<5>::kValue;
+        static constexpr ValueType kClearBufferOutError = GenCode<6>::kValue;
     };
 
     struct Control : CategoryBase<4>
     {
-        static constexpr ValueType kSetDtrError = make(0);
-        static constexpr ValueType kSetRtsError = make(1);
-        static constexpr ValueType kGetModemStatusError = make(2);
-        static constexpr ValueType kSendBreakError = make(3);
-        static constexpr ValueType kGetStateError = make(4);
-        static constexpr ValueType kSetStateError = make(5);
+        static constexpr ValueType kSetDtrError = GenCode<0>::kValue;
+        static constexpr ValueType kSetRtsError = GenCode<1>::kValue;
+        static constexpr ValueType kGetModemStatusError = GenCode<2>::kValue;
+        static constexpr ValueType kSendBreakError = GenCode<3>::kValue;
+        static constexpr ValueType kGetStateError = GenCode<4>::kValue;
+        static constexpr ValueType kSetStateError = GenCode<5>::kValue;
     };
 
     struct Monitor : CategoryBase<5>
     {
-        static constexpr ValueType kMonitorError = make(0);
+        static constexpr ValueType kMonitorError = GenCode<0>::kValue;
     };
 };
 
